06806aaf8f778ebd93d5f193d6c61c839e2d82d8
harshubari <121845828+harshubari@users.noreply.github.com>
2024-03-29 12:26:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,39 @@
 // complete this js code
-function Person(name, age) {
-	Person(name, age,jobTitle){
-		this.name = name;
-		this.age = age;
+/*function Person(name, age){
+	greet(){
+		console.log(`Hello, my name is ${this.name} I am ${this.age} years old.`);
 	}
 }
 
-function Employee(name, age, jobTitle) {
-	return jobGreet () {
-		console.log(`Hello, my name is ${this.name},I am ${this.age} years old, and my job title is ${jobTitle}`);
+function Employee(name, age, jobTitle){
+  Person.apply(this, [name, age]);
+  this.jobTitle = jobTitle;
+	jobGreet(){
+		console.log(`Hello, my name is ${this.name} I am ${this.age} years old, and my job title is ${this.jobTitle}`);
 	}
 }
+Employee.prototype = new Person();*/
+
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.greet = function() { //method using prototype
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+};
+
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age);
+  this.jobTitle = jobTitle;
+}
+
+Employee.prototype = Object.create(Person.prototype);// prototype of person is inherited into employee
+Employee.prototype.constructor = Employee; //get constructor of employee
+
+Employee.prototype.jobGreet = function() {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
