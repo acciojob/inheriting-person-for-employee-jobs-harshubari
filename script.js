caf45c48171cfad4d46460ab8f1ab4ad95437ca6
harshubari <121845828+harshubari@users.noreply.github.com>
2024-03-29 12:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	Person(name, age,jobTitle){
+		this.name = name;
+		this.age = age;
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	return jobGreet () {
+		console.log(`Hello, my name is ${this.name},I am ${this.age} years old, and my job title is ${jobTitle}`);
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
